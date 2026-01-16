67db70ed0563808af3c90e5ed3526c7444f01109
csensoft1 <csensoft@gmail.com>
2026-01-16 06:03:05 -0500

    Initial Commit

---
+++ Personal Tube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Personal Tube
+//
+//  Created by Senthil Kumar Chandrasekaran on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Personal Tube/Personal_TubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_TubeApp.swift
+//  Personal Tube
+//
+//  Created by Senthil Kumar Chandrasekaran on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Personal_TubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
